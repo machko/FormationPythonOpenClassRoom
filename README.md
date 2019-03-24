de2995084aae67c121a41ff12b68d55d90aed8cc
Machko <mehou91@gmail.com>
2019-03-24 17:56:15 +0100

    Modification du Readme pour y inclure la description du 1 projet, Ainsi que l'ajout de main.py contenant les principes et fonction a développer, actuellement aucune fonction d'ajouter

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # FormationPythonOpenClassRoom
 Petit dépôt GitHub pour ma formation en Python sur le site d'OpenClassRoom, pour pouvoir mettre en commun avec mes amis nos expérience sur les TP du cours.
+
+Activité : Créez votre jeu de labyrinthe
+	Première activité du cour, création d'un mini jeu en mode console.
+	
---
+++ main.py
@@ -0,0 +1,26 @@
+#################################################
+#               Script by Machko                #
+#	Licence : GPLv3					            #
+#     Mini jeu : Bot Tux dans le Labyrinthe     #
+#################################################
+
+# Les controleurs sont les suivants : 
+# Q : Quitter, Sauvegarde de la partie en cours
+# N : Déplacement vers le Nord
+# E : Déplacement vers l'Est
+# S : Déplacement vers le Sud
+# O : Déplacement vers l'ouest
+# Suivi d'un nombre de case, 
+#   Par exemple : E5 Déplacement de 5 case vers l'Est
+
+# Variables
+
+# Fonction, Liste des fonctions
+#   Enregistrement automatique a chaque action
+#   Menu avec choix de la carte
+
+
+
+# Début du code
+
+
